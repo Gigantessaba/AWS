14b0b710ae3e57eed74358c84585e393a8c16232
Irlan Santos <irlancdossantos@gmail.com>
2024-10-01 14:46:57 -0400

    sdsafsadsa

--- 001-flask-01-02-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ 001-flask-01-02-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,9 +1,21 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "Hello Would!"
 
+@app.route("/second")
+def second():
+    return "This is the second page!"
 
-
+@app.route("/whichpage/<string:page_id>")
+def whiichpage(page_id):
+    if page_id.isdigit():
+        message = f"The Id of this page is {page_id}!"
+    else:
+        message = f"{page_id} is not a number!"
+    return message
 
 
 if __name__ == '__main__':
