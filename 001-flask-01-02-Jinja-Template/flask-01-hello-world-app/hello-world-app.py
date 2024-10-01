c623abf7ead2995159e5a4c765e9da71ec57712b
IrlanCDosSantos <105889940+Gigantessaba@users.noreply.github.com>
2024-10-01 15:26:50 -0400

    Update hello-world-app.py

--- 001-flask-01-02-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ 001-flask-01-02-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -20,5 +20,5 @@ def whiichpage(page_id):
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=80)
-    # app.run(host= '0.0.0.0', port=8081)
+   # app.run(debug=True, port=80)
+    app.run(host= '0.0.0.0', port=8081)
